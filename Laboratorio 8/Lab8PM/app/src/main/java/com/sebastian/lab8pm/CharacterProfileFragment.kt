11c3158becb4e39166b2ded8776c7571d61ddc96
Teviets <est21405@uvg.edu.gt>
2022-09-18 15:58:19 -0600

    x

--- Laboratorio 8/Lab8PM/app/src/main/java/com/sebastian/lab8pm/CharacterProfileFragment.kt
+++ Laboratorio 8/Lab8PM/app/src/main/java/com/sebastian/lab8pm/CharacterProfileFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.fragment.navArgs
+import coil.load
+import coil.request.CachePolicy
+import coil.transform.CircleCropTransformation
 
 class CharacterProfileFragment : Fragment(R.layout.fragment_character_profile) {
     private lateinit var nameProfileText: TextView
     private lateinit var specieCharText: TextView
     private lateinit var statusCharText: TextView
     private lateinit var genderCharText: TextView
+    private lateinit var imgProfileChar: ImageView
 
     val args: CharacterProfileFragmentArgs by navArgs()
 
@@ -28,6 +33,14 @@ class CharacterProfileFragment : Fragment(R.layout.fragment_character_profile) {
         specieCharText.text = args.place.especie
         statusCharText.text = args.place.Status.toString()
         genderCharText.text = args.place.genero.toString()
+
+        imgProfileChar.load(args.place.imagen){
+            crossfade(true)
+            crossfade(2000)
+            transformations(CircleCropTransformation())
+            memoryCachePolicy(CachePolicy.ENABLED)
+            diskCachePolicy(CachePolicy.ENABLED)
+        }
 
     }
 
